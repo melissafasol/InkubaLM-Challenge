13a7923855b1ef43ed89f13e21c4650cecdbe639
melissafasol <melissaclairefasol@gmail.com>
2025-03-28 13:34:47 +0200

    logit debug

--- utils/ab_testing.py
+++ utils/ab_testing.py
@@ -53,9 +53,10 @@ def create_submission(output_path: str, test_flag: bool) -> pd.DataFrame:
     
         # If log-likelihoods are present, use them
         if "Log-Likelihood" in df.columns:
-            df["Response"] = df["Log-Likelihood"]
-            df["Response"] = df["Response"].apply(process_likelihood).apply(np.argmax)
+            df["logits"] = df["Log-Likelihood"].apply(process_likelihood)
+            df["Response"] = df["logits"].apply(np.argmax)
             print(df[["logits", "Response", "Targets"]].head(10))
+
         
         else:
         # Otherwise, assume it's text output and map it
